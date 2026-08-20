854d9fc475ccff1c635ca4cc73798601c790db28
Andrew Rogers <andygahh@gmail.com>
2026-08-19 22:37:34 -0400

    Initial Commit

---
+++ src/main/java/net/andygahh/caffeineedition/CaffeineEdition.java
@@ -0,0 +1,22 @@
+package net.andygahh.caffeineedition;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CaffeineEdition implements ModInitializer {
+	public static final String MOD_ID = "caffeineedition";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
+	}
+}
---
+++ src/main/java/net/andygahh/caffeineedition/CaffeineEditionDataGenerator.java
@@ -0,0 +1,11 @@
+package net.andygahh.caffeineedition;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CaffeineEditionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
